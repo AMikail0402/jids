6f2ef1261780dfe0e8317e01eab16849e3f61c45
AMikail0402 <alpay.yildiz@outlook.com>
2023-09-09 12:39:09 +0200

    log4j2 konfiguraiton

--- src/main/java/jids/App.java
+++ src/main/java/jids/App.java
@@ -1,19 +1,13 @@
 package jids;
 
-import java.beans.beancontext.BeanContextChildSupport;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.logging.log4j.*;
-
-import jids.util.MessageExtractor;
-
 /**
  * Hello world!
  *
@@ -29,8 +23,14 @@ public class App
 
     public static void main( String[] args ) throws FileNotFoundException, UnsupportedEncodingException, IOException, InterruptedException
     {
+      LocalDateTime dateTime = LocalDateTime.now();
+      DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+      String formattedTime = dateTime.format(dateTimeFormatter);
+      logger.info("Logging");
+   
+    
 
-     // startUpSequence(startUpLogo);
+     //startUpSequence(startUpLogo);
      /*   FileInputStream fis = new FileInputStream("rules.conf");
         InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
         BufferedReader br = new BufferedReader(isr);
@@ -43,10 +43,7 @@ public class App
 
        // System.out.println( RulePatternGenerator.totalRule("TCP source-ip any dest-ip 10.0.0.10 source-port any dest-port 8082"));
      */
-      
-     System.out.println("Sysout");
-    logger.info("Hello world");
-
+    
    
     }
 
