283b126ff6c4e4bce813f12c03058495d80f95e2
AMikail0402 <alpay.yildiz@outlook.com>
2023-11-02 18:13:57 +0100

    Spy-Mocking

--- src/test/java/jids/ObjectTest/RuleObjectTest.java
+++ src/test/java/jids/ObjectTest/RuleObjectTest.java
@@ -1,29 +1,27 @@
 package jids.ObjectTest;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.mockito.Mock;
+
 import jids.Objects.Rule;
 
 public class RuleObjectTest {
-   @Mock
-   static Rule rule = mock(Rule.class);
+   
+   static Rule spyRule = spy(new Rule(null,null,null,null));
 
-   @BeforeAll
-   public static void setUp(){
-    when(rule.getCve()).thenReturn("cve");
-    when(rule.getId()).thenReturn("1");
-    when(rule.getMsg()).thenReturn("msg");
-    when(rule.getPattern()).thenReturn("pattern");
-    when(rule.toString()).thenReturn("pattern | msg | cve | 1");
-   }
+ 
 
     @org.junit.jupiter.api.Test
     public void ruleToStringTest(){
-       Assertions.assertEquals("pattern | msg | cve | 1",rule.toString());
+
+        spyRule.setCve("cve");
+        spyRule.setId("1");
+        spyRule.setMsg("msg");
+        spyRule.setPattern("pattern");
+        
+        Assertions.assertEquals("pattern | msg | cve | 1",spyRule.toString());
     }
     
 }
