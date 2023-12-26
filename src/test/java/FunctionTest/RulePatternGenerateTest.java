df9f77e08032d51c2886a20710fd45956d37d6a4
AMikail0402 <alpay.yildiz@outlook.com>
2023-12-26 18:12:00 +0100

    Test-Ueberarbeitung

--- src/test/java/FunctionTest/RulePatternGenerateTest.java
+++ src/test/java/FunctionTest/RulePatternGenerateTest.java
@@ -10,7 +10,7 @@ public class RulePatternGenerateTest {
     private String validRulePattern1 = "06(?:.|\\n){4}(C0A83801)(C0A83867)(F3CA)(0016)";
     private String validTestRule1 = "TCP source-ip 192.168.56.1 dest-ip 192.168.56.103 source-port 62410 dest-port 22";
     
-    private String zeroPattern = "06(?:.|\\n){4}..(0A00000A)..(1F92)";
+    private String zeroPattern = "06(?:.|\\n){4}.{8}(0A00000A).{4}(1F92)";
     private String zeroRule = "TCP source-ip any dest-ip 10.0.0.10 source-port any dest-port 8082";
 
     @Test
